0ecace311d9611e7a36183c24c2943359c3e1e59
sg <sg@sg-mac.local>
2024-06-20 13:38:15 -0500

    Initial commit

---
+++ test/gh-aws-deploy-oidc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GhAwsDeployOidc from '../lib/gh-aws-deploy-oidc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/gh-aws-deploy-oidc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GhAwsDeployOidc.GhAwsDeployOidcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
